486a7c4d5e34a34f8288b66132191288b5c834c1
jinr-07 <rai.john0707@gmail.com>
2024-10-15 11:30:42 +0545

    feat: changes in candidate component

--- client/src/routes/admin/candidates/candidates.jsx
+++ client/src/routes/admin/candidates/candidates.jsx
@@ -281,7 +281,7 @@ export default function Candidates() {
               <img
                 src={candidate.image}
                 alt={candidate.name}
-                className="w-16 h-auto mr-4 rounded-md bg-cover bg-center"
+                className="w-16 h-auto mr-4 rounded-md object-cover object-center"
               />
               <div className="flex flex-col">
                 <span className="text-lg text-gray-900">{candidate.name}</span>
