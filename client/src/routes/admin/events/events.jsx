3e5c71963c3be1ae0740ac995e0c4e910c619873
jinr-07 <rai.john0707@gmail.com>
2024-10-15 12:28:52 +0545

    feat: changes in event component

--- client/src/routes/admin/events/events.jsx
+++ client/src/routes/admin/events/events.jsx
@@ -7,9 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function Events() {
   const [candidates, setCandidates] = useState([]);
-
   const [events, setEvents] = useState([]);
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
@@ -77,7 +75,6 @@ export default function Events() {
           progress: undefined,
           theme: "light",
         });
-        console.log("Event created:", response.data);
         setEvents([...events, response.data]);
         formik.resetForm();
         setIsModalOpen(false);
@@ -98,28 +95,24 @@ export default function Events() {
   });
 
   return (
-    <div className="pl-80 mx-auto bg-white rounded-lg">
-      <div className="sticky top-0 bg-white shadow-md py-6">
-        <h2 className="text-3xl mb-6 text-gray-800">Events</h2>
-        <button
-          className="mb-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none"
-          onClick={() => setIsModalOpen(true)}
-        >
-          Create New Event
-        </button>
-      </div>
+    <div className="pl-80 mx-auto bg-white rounded-lg shadow-lg p-6">
+      <h2 className="text-3xl mb-6 text-gray-800">Events</h2>
+      <button
+        className="mb-4 px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none shadow"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Create New Event
+      </button>
 
+      {/* Modal for creating new event */}
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="fixed inset-0 bg-black opacity-50" />
           <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-lg z-10">
-            <h3 className="text-xl mb-4">Create Event</h3>
+            <h3 className="text-xl mb-4 font-semibold">Create Event</h3>
             <form onSubmit={formik.handleSubmit} className="space-y-6">
               <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm text-gray-700 mb-2"
-                >
+                <label htmlFor="name" className="block text-sm text-gray-700 mb-2">
                   Event Name
                 </label>
                 <input
@@ -138,17 +131,10 @@ export default function Events() {
               </div>
 
               <div>
-                <label className="block text-sm mb-2">
-                  Select Candidates
-                </label>
-                <div className="flex flex-col space-y-2 h-96 overflow-y-auto">
+                <label className="block text-sm mb-2">Select Candidates</label>
+                <div className="flex flex-col space-y-2 h-96 overflow-y-auto border border-gray-300 p-4 rounded-md">
                   {candidates.map((candidate) => (
-                    <div key={candidate._id} className="flex items-center border-gray-500 border-[1px]">
-                      <img
-                        src={candidate.image}
-                        alt={candidate.name}
-                        className="w-14 h-auto mr-4 object-cover object-center"
-                      />
+                    <div key={candidate._id} className="flex items-center cursor-pointer">
                       <input
                         type="checkbox"
                         id={`candidate-${candidate._id}`}
@@ -168,9 +154,16 @@ export default function Events() {
                           }
                         }}
                       />
-                      <label htmlFor={`candidate-${candidate._id}`} className="text-sm text-gray-700">
-                        <p>{candidate.name}</p>
-                        <p>({candidate.party.name})</p>
+                      <label htmlFor={`candidate-${candidate._id}`} className="flex items-center w-full">
+                        <img
+                          src={candidate.image} // Assuming candidate object contains an image URL
+                          alt={candidate.name}
+                          className="w-12 h-12 rounded-full mr-2" // Use rounded image for better aesthetics
+                        />
+                        <div className="text-sm text-gray-700">
+                          <p>{candidate.name}</p>
+                          <p className="text-gray-500">({candidate.party.name})</p>
+                        </div>
                       </label>
                     </div>
                   ))}
@@ -179,34 +172,42 @@ export default function Events() {
                   <div className="text-red-500 text-sm mt-2">{formik.errors.candidateIds}</div>
                 ) : null}
               </div>
+              <div className="flex items-center justify-between mt-6">
+                <button
+                  type="button"
+                  onClick={() => setIsModalOpen(false)}
+                  className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-md"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="py-2 px-4 border border-transparent rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Create Event
+                </button>
+              </div>
             </form>
-            <div className="flex items-center justify-between mt-6">
-              <button type="button" onClick={() => {
-                setIsModalOpen(false);
-              }} className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-md">Cancel</button>
-              <button
-                type="submit"
-                className="py-2 px-4 border border-transparent rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Create Event
-              </button>
-            </div>
           </div>
         </div>
       )}
 
       <h3 className="text-2xl mt-10 text-gray-800">Existing Events</h3>
-      <ul className="mb-6">
+      <div className="grid grid-cols-1 gap-6">
         {events.length > 0 ? (
           events.map((event) => (
-            <li key={event._id} className="p-3 border-b border-gray-300">
-              {event.name} - Candidates: {event.candidateIds.join(", ")}
-            </li>
+            <div key={event._id} className="bg-gray-100 p-4 rounded-lg shadow-md">
+              <h4 className="font-semibold text-lg">{event.name}</h4>
+              <p className="text-gray-600">
+                Candidates: {event.candidateIds.join(", ")}
+              </p>
+            </div>
           ))
         ) : (
-          <li className="py-3 text-gray-500">No events found.</li>
+          <div className="py-3 text-gray-500">No events found.</div>
         )}
-      </ul>
+      </div>
+
       <ToastContainer />
     </div>
   );
