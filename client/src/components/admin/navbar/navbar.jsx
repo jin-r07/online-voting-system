f7b0252111f7159711bd47614ba0d6e4696c5f54
jinr-07 <rai.john0707@gmail.com>
2024-10-16 11:46:02 +0545

    feat: changes in admin navbar component

--- client/src/components/admin/navbar/navbar.jsx
+++ client/src/components/admin/navbar/navbar.jsx
@@ -30,7 +30,7 @@ export default function AdminNavbar() {
             to="/admin-events"
             className={`flex items-center hover:bg-gray-700 p-3 rounded transition-colors ${location.pathname === '/admin-events' ? 'bg-gray-700' : ''}`}
           >
-            <FaVoteYea className="mr-3" /> Create Events
+            <FaVoteYea className="mr-3" /> Events
           </Link>
         </li>
         <li>
