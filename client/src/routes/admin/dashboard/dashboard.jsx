19bdddb8285bbbb3af50a05448c97eda4ecf8124
jinr-07 <rai.john0707@gmail.com>
2024-10-16 11:46:53 +0545

    feat: changes in admin dashboard component

--- client/src/routes/admin/dashboard/dashboard.jsx
+++ client/src/routes/admin/dashboard/dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
     <div className="ml-64 p-8 bg-gray-100 min-h-screen">
       <div className="mb-10">
         <h1 className="text-4xl font-bold text-gray-800">Admin Dashboard</h1>
-        <p className="text-gray-600 mt-2">Welcome to the electronic voting system. Manage candidates, users, events, and history from here.</p>
+        <p className="text-gray-600 mt-2">Welcome to the electronic voting system. Manage events, candidates, parties, and users from here.</p>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
