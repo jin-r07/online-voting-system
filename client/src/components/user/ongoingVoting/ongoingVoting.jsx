dcbbe34195101cc7976711aab458664bc65d90e1
jinr-07 <rai.john0707@gmail.com>
2024-10-17 14:11:39 +0545

    feat: get ongoing events users side

--- client/src/components/user/ongoingVoting/ongoingVoting.jsx
+++ client/src/components/user/ongoingVoting/ongoingVoting.jsx
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function OngoingVoting() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const votingData = [
-        { title: "Ongoing Vote 1" },
-        { title: "Ongoing Vote 2" },
-        { title: "Ongoing Vote 3" },
-    ];
+    const [ongoingEvents, setOngoingEvents] = useState("");
 
     const handlePrevClick = () => {
         setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? votingData.length - 1 : prevIndex - 1
+            prevIndex === 0 ? ongoingEvents.length - 1 : prevIndex - 1
         );
     };
 
     const handleNextClick = () => {
         setCurrentIndex((prevIndex) =>
-            prevIndex === votingData.length - 1 ? 0 : prevIndex + 1
+            prevIndex === ongoingEvents.length - 1 ? 0 : prevIndex + 1
         );
     };
 
@@ -30,12 +29,35 @@ export default function OngoingVoting() {
         return () => clearInterval(interval);
     }, []);
 
+
+    const fetchOngoingEvents = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/api-admin/get-events");
+            setOngoingEvents(response.data);
+        } catch (err) {
+            toast.error("Error fetching candidates", {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+    };
+
+    useEffect(() => {
+        fetchOngoingEvents();
+    }, []);
+
     return (
         <div className="w-full h-full bg-[#9BF00B] lg:px-28 px-2 py-4 text-center lg:text-lg tracking-wide">
-            {votingData.length === 0 ? (
+            {ongoingEvents.length === 0 ? (
                 <p>No ongoing votes at the moment.</p>
-            ) : votingData.length === 1 ? (
-                <p>{votingData[0].title}</p>
+            ) : ongoingEvents.length === 1 ? (
+                <p>{ongoingEvents[0].eventName}</p>
             ) : (
                 <>
                     <div className="slider-container overflow-hidden relative">
@@ -64,6 +86,7 @@ export default function OngoingVoting() {
                     </div>
                 </>
             )}
+            <ToastContainer />
         </div>
     );
 }
