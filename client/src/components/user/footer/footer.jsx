991b2f241c823a1c2e875866bb90a7e1d8904f19
jinr-07 <rai.john0707@gmail.com>
2024-10-18 14:08:54 +0545

    feat: changes in footer component

--- client/src/components/user/footer/footer.jsx
+++ client/src/components/user/footer/footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa';
 import { BsTwitterX } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
@@ -11,9 +12,9 @@ export default function Footer() {
                     <p className="text-sm">&copy; {new Date().getFullYear()} All rights reserved.</p>
                 </div>
                 <div className="flex flex-col md:flex-row items-center text-base">
-                    <a href="/privacy-policy" className="mx-2 hover:underline">Privacy Policy</a>
-                    <a href="/terms-of-service" className="mx-2 hover:underline">Terms of Service</a>
-                    <a href="/contact" className="mx-2 hover:underline">Contact Us</a>
+                    <Link to="/privacy-policy" className="mx-2 hover:underline">Privacy Policy</Link>
+                    <Link to="/terms-of-service" className="mx-2 hover:underline">Terms of Service</Link>
+                    <Link to="/contact" className="mx-2 hover:underline">Contact Us</Link>
                 </div>
                 <div className="flex mt-4 md:mt-0">
                     <a href="https://www.facebook.com/JaeJin2003/" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-blue-500">
@@ -22,7 +23,7 @@ export default function Footer() {
                     <a href="https://x.com/John0707Rai" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-blue-400">
                         <BsTwitterX />
                     </a>
-                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-blue-600">
+                    <a href="https://www.linkedin.com/in/john-hang-rai-b844a7266/" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-blue-600">
                         <FaLinkedin />
                     </a>
                 </div>
