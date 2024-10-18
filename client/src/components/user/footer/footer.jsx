d10f9c391bb8f782903dbde35ed607e60e8f8e15
jinr-07 <rai.john0707@gmail.com>
2024-10-18 14:00:19 +0545

    feat: footer component

---
+++ client/src/components/user/footer/footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white py-4 mt-10 w-full h-full lg:px-28 px-4">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+                <div className="mb-4 md:mb-0">
+                    <h4 className="text-lg">Electronic Voting System (EVS)</h4>
+                    <p className="text-sm">&copy; {new Date().getFullYear()} All rights reserved.</p>
+                </div>
+                <div className="flex flex-col md:flex-row items-center text-base">
+                    <a href="/privacy-policy" className="mx-2 hover:underline">Privacy Policy</a>
+                    <a href="/terms-of-service" className="mx-2 hover:underline">Terms of Service</a>
+                    <a href="/contact" className="mx-2 hover:underline">Contact Us</a>
+                </div>
+                <div className="flex mt-4 md:mt-0">
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-blue-500">
+                        <FaFacebook />
+                    </a>
+                    <a href="https://www.x.com" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-blue-400">
+                        <FaTwitter />
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-blue-600">
+                        <FaLinkedin />
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="mx-2 text-xl hover:text-pink-500">
+                        <FaInstagram />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
--- client/src/routes/user/vote/votePage.jsx
+++ client/src/routes/user/vote/votePage.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { capitalizeFirstLetter } from "../../../utils/capitalizeFirstLetter";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "../../../components/user/footer/footer";
 
 export default function VotePage() {
     const { eventId } = useParams();
@@ -65,75 +66,78 @@ export default function VotePage() {
     };
 
     return (
-        <div className="mb-8 lg:px-28 px-4 pt-10 bg-gray-50 w-full h-full">
-            <h2 className="text-3xl font-semibold mb-8">Event: {eventData.eventName}</h2>
-            <div className="mb-4 text-xl">
-                <p><strong>Status:&nbsp;</strong><span>{capitalizeFirstLetter(eventData.status)}</span></p>
-                <p><strong>Start Date:&nbsp;</strong><span>{startDate}</span></p>
-                <p><strong>End Date:&nbsp;</strong><span>{endDate}</span></p>
-            </div>
-            <h2 className="text-xl font-semibold my-8">All Candidates:</h2>
-            <ul className="space-y-6">
-                {eventData.candidates.map((candidate) => (
-                    <li key={candidate._id} className="flex items-center p-4 bg-white shadow-lg rounded-lg border border-gray-200">
-                        <img src={candidate.partyImage} alt={candidate.party.name} className="w-20 h-auto rounded-md mr-4" />
-                        <div className="flex-1">
-                            <h3 className="text-xl font-semibold text-gray-800 mb-1">{candidate.party.name}</h3>
-                            <div className="flex items-center">
-                                <img src={candidate.image} alt={candidate.name} className="w-10 h-auto rounded-md mr-2" />
-                                <p className="text-gray-700">{candidate.name}</p>
-                            </div>
-                        </div>
-                        <button
-                            className="mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-200"
-                            onClick={() => openModal(candidate)}
-                        >
-                            Vote
-                        </button>
-                    </li>
-                ))}
-            </ul>
-
-            {isModalOpen && (
-                <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-                    <div className="bg-white rounded-lg shadow-lg p-6 w-96 h-auto mx-auto">
-                        <h2 className="text-xl mb-4 text-center font-semibold">Confirm Your Vote?</h2>
-                        {selectedCandidate && (
-                            <div className="text-center my-12">
-                                <div className="flex flex-col items-center justify-center mb-4">
-                                    {selectedCandidate.partyImage && (
-                                        <img
-                                            src={selectedCandidate.partyImage}
-                                            alt={selectedCandidate.party.name}
-                                            className="w-36 h-auto rounded-md mr-4"
-                                        />
-                                    )}
-                                    <h3 className="text-xl font-semibold mt-4">{selectedCandidate.party.name}</h3>
-                                    <h3 className="text-lg mt-4">Candidate: {selectedCandidate.name}</h3>
+        <>
+            <div className="mb-8 lg:px-28 px-4 pt-10 bg-gray-50 w-full h-full">
+                <h2 className="text-3xl font-semibold mb-8">Event: {eventData.eventName}</h2>
+                <div className="mb-4 text-xl">
+                    <p><strong>Status:&nbsp;</strong><span>{capitalizeFirstLetter(eventData.status)}</span></p>
+                    <p><strong>Start Date:&nbsp;</strong><span>{startDate}</span></p>
+                    <p><strong>End Date:&nbsp;</strong><span>{endDate}</span></p>
+                </div>
+                <h2 className="text-xl font-semibold my-8">All Candidates:</h2>
+                <ul className="space-y-6">
+                    {eventData.candidates.map((candidate) => (
+                        <li key={candidate._id} className="flex items-center p-4 bg-white shadow-lg rounded-lg border border-gray-200">
+                            <img src={candidate.partyImage} alt={candidate.party.name} className="w-20 h-auto rounded-md mr-4" />
+                            <div className="flex-1">
+                                <h3 className="text-xl font-semibold text-gray-800 mb-1">{candidate.party.name}</h3>
+                                <div className="flex items-center">
+                                    <img src={candidate.image} alt={candidate.name} className="w-10 h-auto rounded-md mr-2" />
+                                    <p className="text-gray-700">{candidate.name}</p>
                                 </div>
                             </div>
-                        )}
-                        <div className="flex justify-between mt-6">
                             <button
-                                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-200"
-                                onClick={closeModal}
+                                className="mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-200"
+                                onClick={() => openModal(candidate)}
                             >
-                                Cancel
+                                Vote
                             </button>
-                            <button
-                                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-200"
-                                onClick={() => {
-                                    toast.success(`You voted for ${selectedCandidate.name}!`);
-                                    closeModal();
-                                }}
-                            >
-                                Confirm Vote
-                            </button>
+                        </li>
+                    ))}
+                </ul>
+
+                {isModalOpen && (
+                    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
+                        <div className="bg-white rounded-lg shadow-lg p-6 w-96 h-auto mx-auto">
+                            <h2 className="text-xl mb-4 text-center font-semibold">Confirm Your Vote?</h2>
+                            {selectedCandidate && (
+                                <div className="text-center my-12">
+                                    <div className="flex flex-col items-center justify-center mb-4">
+                                        {selectedCandidate.partyImage && (
+                                            <img
+                                                src={selectedCandidate.partyImage}
+                                                alt={selectedCandidate.party.name}
+                                                className="w-36 h-auto rounded-md mr-4"
+                                            />
+                                        )}
+                                        <h3 className="text-xl font-semibold mt-4">{selectedCandidate.party.name}</h3>
+                                        <h3 className="text-lg mt-4">Candidate: {selectedCandidate.name}</h3>
+                                    </div>
+                                </div>
+                            )}
+                            <div className="flex justify-between mt-6">
+                                <button
+                                    className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-200"
+                                    onClick={closeModal}
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-200"
+                                    onClick={() => {
+                                        toast.success(`You voted for ${selectedCandidate.name}!`);
+                                        closeModal();
+                                    }}
+                                >
+                                    Confirm Vote
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
-            <ToastContainer />
-        </div>
+                )}
+                <ToastContainer />
+            </div>
+            <Footer />
+        </>
     );
 }
