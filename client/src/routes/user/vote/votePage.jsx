c49f248aa9dfb4adf9920708d1fa171a5e936db6
jinr-07 <rai.john0707@gmail.com>
2024-10-19 19:00:40 +0545

    feat: changes in vote dynamic route

--- client/src/routes/user/vote/votePage.jsx
+++ client/src/routes/user/vote/votePage.jsx
@@ -58,7 +58,7 @@ export default function VotePage() {
     return (
         <>
             <div className="mb-8 lg:px-28 px-4 pt-10 bg-gray-50 w-full h-full">
-                <h2 className="xl:text-2xl text-xl font-semibold mb-8">Event: {eventData.eventName}</h2>
+                <h2 className="xl:text-2xl text-xl font-semibold mb-8">{eventData.eventName}</h2>
                 <div className="mb-4 xl:text-xl text-lg">
                     <p><strong>Status:&nbsp;</strong><span>{capitalizeFirstLetter(eventData.status)}</span></p>
                     <p><strong>Start Date:&nbsp;</strong><span>{startDate}</span></p>
