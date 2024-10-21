a9383be5fe343f8655ce564f9ce412d5a8aab49f
jinr-07 <rai.john0707@gmail.com>
2024-10-21 12:47:09 +0545

    feat: minor changesin vote component

--- client/src/routes/user/vote/votePage.jsx
+++ client/src/routes/user/vote/votePage.jsx
@@ -121,10 +121,12 @@ export default function VotePage() {
                                 <img src={candidate.partyImage} alt={candidate.party.name} className="w-20 h-auto rounded-sm mr-4" />
                                 <div className="flex-1">
                                     <h3 className="lg:text-xl text-lg font-semibold text-gray-800 mb-1">{candidate.party.name}</h3>
-                                    <p className="text-base text-gray-500">Votes: {totalVotes}</p>
                                     <div className="flex items-center">
                                         <img src={candidate.image} alt={candidate.name} className="w-10 h-auto mr-2 rounded-md" />
-                                        <p className="text-gray-700">{candidate.name}</p>
+                                        <div>
+                                            <p className="text-gray-700">{candidate.name}</p>
+                                            <p className="text-base">Votes: {totalVotes}</p>
+                                        </div>
                                     </div>
                                 </div>
                                 <button
