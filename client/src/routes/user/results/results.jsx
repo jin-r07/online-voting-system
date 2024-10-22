0d42648046c651cbda5727637a60c60a67ccdbf9
jinr-07 <rai.john0707@gmail.com>
2024-10-22 12:33:38 +0545

    feat: minor changes in results component

--- client/src/routes/user/results/results.jsx
+++ client/src/routes/user/results/results.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Footer from "../../../components/user/footer/footer";
 
 export default function Results() {
     return (
-        <div>Results</div>
+        <div className="w-full h-full">
+            Results
+            <Footer />
+        </div>
     );
 }
