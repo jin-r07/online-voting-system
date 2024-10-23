dbc6e62ae405ac77f38baa0b3b76ccfa20b910b0
jinr-07 <rai.john0707@gmail.com>
2024-10-23 13:19:42 +0545

    feat: dynamic vote results component

--- client/src/routes/user/results/results.jsx
+++ client/src/routes/user/results/results.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { capitalizeFirstLetter } from "../../../utils/capitalizeFirstLetter";
 import { useToast } from "../../../context/toast";
-import { LuUserCheck2 } from "react-icons/lu";
+import { LuVote } from "react-icons/lu";
 import { FaRegTimesCircle } from "react-icons/fa";
 import { RxDownload } from "react-icons/rx";
 import { formatDate } from "../../../utils/formatDate&Time";
@@ -40,7 +40,7 @@ export default function Results() {
     return (
         <div className="w-full h-screen flex flex-col bg-gradient-to-b from-gray-100 via-white to-gray-50">
             <div className="flex-grow py-10 px-4 lg:px-28">
-                <h2 className="text-3xl font-extrabold mb-8">Completed Vote Events Results</h2>
+                <h2 className="lg:text-3xl text-xl font-extrabold mb-8">Completed Vote Events Results</h2>
 
                 {eventData.length > 0 ? (
                     <div className="w-full">
@@ -50,13 +50,15 @@ export default function Results() {
                                 className="bg-white p-6 border-black border-[1px] rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300"
                             >
                                 <div className="flex justify-between items-center">
-                                    <h3 className="text-2xl font-semibold text-gray-900">{event.eventName}</h3>
                                     <div className="flex items-center">
                                         {event.status === "completed" ? (
-                                            <LuUserCheck2 className="text-green-500 text-3xl" />
+                                            <LuVote className="text-green-500 text-3xl" />
                                         ) : (
                                             <FaRegTimesCircle className="text-red-500 text-2xl" />
                                         )}
+                                        <h3 className="ml-4 lg:text-2xl text-lg font-semibold text-gray-900">{event.eventName}</h3>
+                                    </div>
+                                    <div className="flex items-center">
                                         <button className="bg-blue-600 ml-6 p-2.5 rounded-md hover:bg-blue-700 transition-all duration-300 hover:scale-105">
                                             <RxDownload className="text-xl text-white" />
                                         </button>
