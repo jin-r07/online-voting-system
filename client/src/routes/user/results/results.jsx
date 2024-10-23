81e2142762c68b4b73aff08b9d933184f1e8185f
jinr-07 <rai.john0707@gmail.com>
2024-10-23 13:03:59 +0545

    feat: minor changes in results component

--- client/src/routes/user/results/results.jsx
+++ client/src/routes/user/results/results.jsx
@@ -4,6 +4,7 @@ import { capitalizeFirstLetter } from "../../../utils/capitalizeFirstLetter";
 import { useToast } from "../../../context/toast";
 import { LuUserCheck2 } from "react-icons/lu";
 import { FaRegTimesCircle } from "react-icons/fa";
+import { RxDownload } from "react-icons/rx";
 import { formatDate } from "../../../utils/formatDate&Time";
 import Footer from "../../../components/user/footer/footer";
 
@@ -35,34 +36,28 @@ export default function Results() {
                 <h2 className="text-3xl font-extrabold mb-8">Completed Vote Events Results</h2>
 
                 {eventData.length > 0 ? (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <div className="w-full">
                         {eventData.map((event) => (
                             <div
                                 key={event._id}
                                 className="bg-white p-6 border-black border-[1px] rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300"
                             >
-                                <div className="flex justify-between items-center mb-4">
+                                <div className="flex justify-between items-center">
                                     <h3 className="text-2xl font-semibold text-gray-900">{event.eventName}</h3>
-                                    {event.status === "completed" ? (
-                                        <LuUserCheck2 className="text-blue-500 text-3xl" />
-                                    ) : (
-                                        <FaRegTimesCircle className="text-red-500 text-2xl" />
-                                    )}
-                                </div>
-                                <p className="text-gray-600 text-lg mb-2">
-                                    <strong>Status:</strong>&nbsp;{capitalizeFirstLetter(event.status)}
-                                </p>
-                                <p className="text-gray-600 text-lg mb-2">
-                                    <strong>Start Date:</strong>&nbsp;{formatDate(event.start)}
-                                </p>
-                                <p className="text-gray-600 text-lg mb-2">
-                                    <strong>End Date:</strong>&nbsp;{formatDate(event.end)}
-                                </p>
-                                <div className="flex justify-end">
-                                    <button
-                                        className="mt-6 py-2 px-6 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-all duration-300 hover:scale-105">
-                                        Vote Now
-                                    </button>
+                                    <div className="flex items-center">
+                                        {event.status === "completed" ? (
+                                            <LuUserCheck2 className="text-green-500 text-3xl" />
+                                        ) : (
+                                            <FaRegTimesCircle className="text-red-500 text-2xl" />
+                                        )}
+                                        <button className="bg-blue-600 ml-6 p-2.5 rounded-md hover:bg-blue-700 transition-all duration-300 hover:scale-105">
+                                            <RxDownload className="text-xl text-white" />
+                                        </button>
+                                        <button
+                                            className="ml-3 py-2 px-6 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-all duration-300 hover:scale-105">
+                                            View Details
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         ))}
